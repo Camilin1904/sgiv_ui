f3f14e25ba99ec0322ed91fe47ff6ccb2b3468dd
Santiago0212004 <sjbs0212@gmail.com>
2024-05-20 23:55:15 -0500

    plans

--- src/items/PlanItem.js
+++ src/items/PlanItem.js
@@ -3,21 +3,18 @@ import '../css/items/PlanItem.css';
 
 function PlanItem(props) {
     return (
-      <li className='PlanItem'>
-        <h2 className='PlanItem-name'>{props.name}</h2>
+      <div className='PlanItem'>
         <p className='PlanItem-id'>ID: {props.id}</p>
         <p className='PlanItem-code'>Code: {props.code}</p>
-        <p className='PlanItem-requestDescription'>Request Description: {props.requestDescription}</p>
         <p className='PlanItem-numberOfPeople'>Number of People: {props.numberOfPeople}</p>
-        <p className='PlanItem-requestDate'>Request Date: {props.requestDate.toDateString()}</p>
         <p className='PlanItem-startDate'>Start Date: {props.startDate.toDateString()}</p>
         <p className='PlanItem-endDate'>End Date: {props.endDate.toDateString()}</p>
         <p className='PlanItem-totalValue'>Total Value: ${props.totalValue}</p>
         <p className='PlanItem-creationDate'>Creation Date: {props.creationDate.toDateString()}</p>
         <p className='PlanItem-status'>Status: {props.status}</p>
         <p className='PlanItem-client'>Client: {props.client.firstName} {props.client.lastName}</p>
-        <p className='PlanItem-user'>User: {props.user.username}</p>
-      </li>
+        <p className='PlanItem-user'>Creator user: {props.user.username}</p>
+      </div>
     );
 }
 
--- src/screens/VistaPlanes.js
+++ src/screens/VistaPlanes.js
@@ -3,140 +3,150 @@ import '../css/vistaPlanes.css';
 import logo from '../img/logo.png';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import { PlanItem } from '../items/PlanItem';
 
-
-function ViewPlan(){
-    const navigate = useNavigate()
-    const username = localStorage.getItem('username')
-    const token = localStorage.getItem('token')
-
-    const handleLogout = () => {
-      navigate('/')
-    };
-
+function ViewPlan() {
+    const navigate = useNavigate();
     const [plans, setPlans] = useState([]);
-
     const [num, setNum] = useState(0);
+    const token = localStorage.getItem('token');
 
-    useEffect(() =>{
-      const fetchPlans = async () => {
-        try {
-          const response = await axios.post('http://localhost:9092/plan/page_plan', {
-            headers: {
-              'Authorization': `Bearer ${token}`
-            },
-            body : {
-              "code":null,
-              "tvm":null,
-              "tvl":null,
-              "clientName":null,
-              "status":"Active",
-              "size":10,
-              "page":1,
+    useEffect(() => {
+        const fetchPlans = async () => {
+            try {
+                const response = await axios.post(
+                    'http://localhost:9092/plan/page_plan',
+                    {
+                        code: null,
+                        tvm: null,
+                        tvl: null,
+                        clientName: null,
+                        status: 'Active',
+                        size: 10,
+                        page: 0,
+                    },
+                    {
+                        headers: {
+                            Authorization: `Bearer ${token}`,
+                            'Content-Type': 'application/json',
+                        },
+                    }
+                );
+                setPlans(response.data.content);
+            } catch (error) {
+                console.log(error);
             }
-          });
-          setPlans(response.data);
-        } catch (error) {
-          console.log(error);
-        } finally {
-          //setLoading(false);
-        } 
-      };
+        };
 
-      const countPlans = async () => {
-        try {
-          const response = await axios.get('http://localhost:9092/plan/count', {
-            headers: {
-              'Authorization': `Bearer ${token}`
+        const countPlans = async () => {
+            try {
+                const response = await axios.get('http://localhost:9092/plan/count', {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                });
+                setNum(response.data);
+            } catch (error) {
+                console.log(error);
             }
-          });
-          setNum(response.data.name);
-        } catch (error) {
-          console.log(error);
-        } finally {
-          //setLoading(false);
-        }
-      };
-      
-      countPlans()
-      fetchPlans()
-      
-    })
+        };
 
+        countPlans();
+        fetchPlans();
+    }, [token]);
 
-    
-
+    console.log(plans);
+    console.log(num);
 
     return (
-      <body id="ii2r">
-      <div id="i10d" class="gjs-row">
-        <div id="ilv1" class="gjs-cell">
-          <div class="gjs-row" id="ibzy2">
-            <div class="gjs-cell" id="i526g">
-              <img id="ijcaw" src={logo}/>
-              </div>
-            <div class="gjs-cell" id="ikt9w">
-              <div id="iah6o2">Planes
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div id="ipbh" class="gjs-row">
-        <div class="gjs-cell">
-          <div id="igqg" class="gjs-row">
-            <div id="ikv4" class="gjs-cell">
-              <div id="ilxmp" class="gjs-row">
-                <div id="i1axm" class="gjs-cell">
-                  <div id="iwo55" class="gjs-row">
-                    <div id="i4o0i" class="gjs-cell">
-                      <div class="gjs-row" id="i6rii7">
-                        <div class="gjs-cell" id="i0ak2w">
-                          <input type="text" id="im30us" placeholder="Plan"/>
+        <div id="ii2r">
+            <div id="i10d" className="gjs-row">
+                <div id="ilv1" className="gjs-cell">
+                    <div className="gjs-row" id="ibzy2">
+                        <div className="gjs-cell" id="i526g">
+                            <img id="ijcaw" src={logo} alt="Logo" />
                         </div>
-                        <div class="gjs-cell" id="idgqzc">
-                          <button type="button" id="i1aomg"></button>
+                        <div className="gjs-cell" id="ikt9w">
+                            <div id="iah6o2">Planes</div>
                         </div>
-                        <div class="gjs-cell" id="ivtoj3">
-                          <button type="button" id="ivslaq">Crear</button>
-                        </div>
-                      </div>
                     </div>
-                  </div>
-                  <div class="gjs-row" id="ilh64g">
-                    <div class="gjs-cell" id="izbov7">
-                    </div>
-                  </div>
-                  <div class="gjs-row" id="i39ss1">
-                    <div class="gjs-cell" id="istsko">
-                      <div class="gjs-row" id="iu7isy">
-                        <div class="gjs-cell" id="i6hi6h">
-                          <button type="button" id="if0ynj">Anterior</button>
-                        </div>
-                        <div class="gjs-cell" id="ixsnst">
-                          <div class="gjs-row" id="i89pat">
-                            <div class="gjs-cell" id="ihtxki">
-                              <input type="text" id="i46s8v"/>
-                            </div>
-                            <div class="gjs-cell" id="i9i75f">
-                              <button type="button" id="inpm0j"></button>
-                            </div>
-                          </div>
-                        </div>
-                        <div class="gjs-cell" id="izlfvz">
-                          <button type="button" id="isdsj6">Siguiente</button>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
                 </div>
-              </div>
             </div>
-          </div>
+            <div id="ipbh" className="gjs-row">
+                <div className="gjs-cell">
+                    <div id="igqg" className="gjs-row">
+                        <div id="ikv4" className="gjs-cell">
+                            <div id="ilxmp" className="gjs-row">
+                                <div id="i1axm" className="gjs-cell">
+                                    <div id="iwo55" className="gjs-row">
+                                        <div id="i4o0i" className="gjs-cell">
+                                            <div className="gjs-row" id="i6rii7">
+                                                <div className="gjs-cell" id="i0ak2w">
+                                                    <input type="text" id="im30us" placeholder="Plan" />
+                                                </div>
+                                                <div className="gjs-cell" id="idgqzc">
+                                                    <button type="button" id="i1aomg"></button>
+                                                </div>
+                                                <div className="gjs-cell" id="ivtoj3">
+                                                    <button type="button" id="ivslaq">Crear</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="gjs-row" id="ilh64g">
+                                        <div className="gjs-cell" id="izbov7">
+                                          <div className="gjs-row" id="plan-list">
+                                              {plans.map((plan) => (
+                                                  <PlanItem
+                                                      key={plan.id}
+                                                      id={plan.id}
+                                                      name={plan.name}
+                                                      code={plan.code}
+                                                      requestDescription={plan.requestDescription}
+                                                      numberOfPeople={plan.numberOfPeople}
+                                                      requestDate={new Date(plan.requestDate)}
+                                                      startDate={new Date(plan.startDate)}
+                                                      endDate={new Date(plan.endDate)}
+                                                      totalValue={plan.totalValue}
+                                                      creationDate={new Date(plan.creationDate)}
+                                                      status={plan.status}
+                                                      client={plan.client}
+                                                      user={plan.user}
+                                                  />
+                                              ))}
+                                        </div>
+                                        </div>
+                                    </div>
+                                    <div className="gjs-row" id="i39ss1">
+                                        <div className="gjs-cell" id="istsko">
+                                            <div className="gjs-row" id="iu7isy">
+                                                <div className="gjs-cell" id="i6hi6h">
+                                                    <button type="button" id="if0ynj">Anterior</button>
+                                                </div>
+                                                <div className="gjs-cell" id="ixsnst">
+                                                    <div className="gjs-row" id="i89pat">
+                                                        <div className="gjs-cell" id="ihtxki">
+                                                            <input type="text" id="i46s8v" />
+                                                        </div>
+                                                        <div className="gjs-cell" id="i9i75f">
+                                                            <button type="button" id="inpm0j"></button>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div className="gjs-cell" id="izlfvz">
+                                                    <button type="button" id="isdsj6">Siguiente</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </body>
     );
 }
 
-export {ViewPlan}
+export { ViewPlan };
